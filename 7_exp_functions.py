2bfd73acb104198ef02513a54a63b872ecb82f92
Arie Twigt <twigt.arie@gmail.com>
2023-05-26 10:08:47 +0200

    added examples for building a function

---
+++ 7_exp_functions.py
@@ -0,0 +1,44 @@
+#%%
+import math
+from typing import Tuple
+
+
+def calc_pythagoras(a:float, 
+                    b:float, 
+                    rounding=1, 
+                    double=1) -> Tuple[float, float]:
+    '''
+    Function to apply the pythagoras theorem
+
+    parameters:
+    * a
+    * b
+
+    returns:
+    * c, c_sqrd
+    '''
+    
+    # check the input data types
+    required_types = (int, float)
+
+    if type(a) not in required_types:
+        raise TypeError(f"The value {a} should be numeric. Got {type(5)}")
+
+
+    if rounding < 1:
+        raise ValueError(f"The rounding value should be higher than 1. Got {rounding}")
+
+    c_sqrd = math.pow(a, 2) + math.pow(b, 2)
+    c = math.sqrt(c_sqrd)
+    c_rounded = round(c, rounding) * double
+    return c_rounded, c_sqrd
+
+
+#%%
+my_c, my_c_sqr = calc_pythagoras(a=5, 
+                       b=20,
+                       rounding=1,
+                       double=2)
+print(my_c)
+
+# %%
